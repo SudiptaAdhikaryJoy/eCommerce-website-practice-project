b593388f83a706079223bf9d02bf8f160f1df52a
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-07-11 00:11:55 +0600

    add husky

--- src/views/pages/common/Header.tsx
+++ src/views/pages/common/Header.tsx
@@ -1,3 +1,8 @@
+import logo from "../../../assets/images/Logo.png";
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="container mx-auto">
+      <img src={logo} alt="" />
+    </div>
+  );
 };
